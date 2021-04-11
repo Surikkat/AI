752e7e8109c3d59d52043c881d6e254df225b844
Surikkat <74894572+Surikkat@users.noreply.github.com>
2021-04-11 10:50:03 +0300

    Add files via upload

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,3 +21,36 @@ void MainWindow::on_LoadFile_clicked()
     QPixmap pix = QPixmap::fromImage(original);
     ui->OriginalIMG->setPixmap(pix);
 }
+
+double MainWindow::similatingPix(const QColor& a,const QColor& b) const
+{
+    double simPixels = 0;
+
+    simPixels += abs(a.red()-b.red());
+    simPixels += abs(a.green()-b.green());
+    simPixels += abs(a.blue()-b.blue());
+    simPixels += abs(a.alpha()-b.alpha());
+
+    return 1 - (simPixels/(255*4.0f));
+}
+
+bool MainWindow::isBetter(const QRect& rect, const QColor color) const
+{
+    double currentSim = 0;
+    double updateSim = 0;
+
+    for(int y = rect.top();y != rect.top()+rect.height();y++){
+        for(int x=rect.left();x!=rect.left()+rect.width();x++){
+            currentSim += similatingPix(original.pixelColor(x,y), result.pixelColor(x,y));
+            updateSim += similatingPix(original.pixelColor(x,y), color);
+        }
+    }
+
+    //Нормализуем (0-1)
+    currentSim = currentSim/(rect.height()*rect.width());
+    updateSim = updateSim/(rect.height()*rect.width());
+
+    return (updateSim>currentSim);
+}
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -4,6 +4,10 @@
 #include <QMainWindow>
 #include <QFileDialog>
 #include <QPixmap>
+#include <QRandomGenerator>
+#include <QColor>
+#include <cmath>
+#include <QRect>
 
 namespace Ui { class MainWindow; }
 
@@ -16,14 +20,30 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void step();//Шаг алгоритма
+    int getWidth()const;//Геттер ширины
+    int getHeight()const;//Геттер высоты
+
 private slots:
-    void on_LoadFile_clicked();
+    void on_LoadFile_clicked();//Загрузка изображения
 
 private:
     Ui::MainWindow *ui;
-    QString fileName;
-    QImage original;
-    QImage result;
+    QString fileName;//Имя файла с изображением
+    QImage original;//Оригинальное изображение
+    QImage result;//Изображение, сгенерированное алгоритмом
+    int width;//Ширина изображения
+    int height;//Высота изображения
+
+    QRandomGenerator generator;//Генератор случайных чисел
+    QRandomGenerator randomWidth;//Случайная ширина(в каком-то диапазоне)
+    QRandomGenerator randomHeight;//Случайная высота(в каком-то диапазоне)
+    QRandomGenerator randomColor;//Случайный цвет(в каком-то диапазоне)
+
+    void draw();//Функция рисования
+    bool isBetter(const QRect& rect,const QColor color)const;//Получилось лучше?
+    double similatingPix(const QColor& a,const QColor& b)const;//Насколько похожи пиксели
+    QRect createRandomRectangle()const;//Создать случайный прямоугольник
 };
 
 #endif // MAINWINDOW_H
